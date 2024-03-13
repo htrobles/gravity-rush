87c368acc9e4812e0f9ae3bfe7648ce794f0b080
Hector Robles <h_robles@fanshaweonline.ca>
2024-03-13 01:23:58 -0400

    refactor reset game

--- Physics.js
+++ Physics.js
@@ -1,10 +1,10 @@
 import Matter from "matter-js";
 import Constants from "./Constants";
 import { getY, roll } from "./_utils";
-import { pushPlayer, switchGravity } from "./gameFunctions";
+import { pushPlayer, resetGame, switchGravity } from "./gameFunctions";
 
 const Physics = (entities, { touches, dispatch, events, time }) => {
-  let engine = entities.physics.engine;
+  const engine = entities.physics.engine;
 
   // Obstacle Movement
   for (let i = 1; i <= 1; i++) {
@@ -44,44 +44,7 @@ const Physics = (entities, { touches, dispatch, events, time }) => {
           pushPlayer(entities.Player);
           break;
         case "reset-game":
-          Matter.Body.setPosition(entities.Player.body, {
-            x: Constants.SCREEN_WIDTH / 2,
-            y: Constants.SCREEN_HEIGHT / 2,
-          });
-          Matter.Body.setVelocity(entities.Player.body, { x: 0, y: 0 });
-
-          for (let i = 1; i <= 3; i++) {
-            const box = entities[`Obstacle${i}`];
-
-            let x;
-
-            switch (i) {
-              case 1:
-                x = Constants.SCREEN_WIDTH + Constants.OBSTACLE_HEIGHT / 2;
-                break;
-              case 2:
-                x =
-                  Constants.SCREEN_WIDTH * 1.385 +
-                  Constants.OBSTACLE_HEIGHT / 2;
-                break;
-              case 3:
-                x =
-                  Constants.SCREEN_WIDTH * 1.77 + Constants.OBSTACLE_HEIGHT / 2;
-                break;
-              default:
-                break;
-            }
-
-            Matter.Body.setPosition(box.body, {
-              x: x,
-              y: getY(),
-            });
-            box.isDangerous = false;
-            box.body.isDangerous = false;
-            engine.world.gravity.y = 0.5;
-
-            dispatch({ type: "pause" });
-          }
+          resetGame(entities, dispatch);
           break;
         default:
           break;
--- gameFunctions.js
+++ gameFunctions.js
@@ -1,4 +1,6 @@
 import Matter from "matter-js";
+import Constants from "./Constants";
+import { getY } from "./_utils";
 
 export function switchGravity(engine) {
   if (!engine.world.gravity.y) {
@@ -10,4 +12,44 @@ export function switchGravity(engine) {
 
 export function pushPlayer(entity) {
   Matter.Body.applyForce(entity.body, entity.body.position, { x: 0.125, y: 0 });
+}
+
+export function resetGame(entities, dispatch) {
+  const engine = entities.physics.engine;
+
+  Matter.Body.setPosition(entities.Player.body, {
+    x: Constants.SCREEN_WIDTH / 2,
+    y: Constants.SCREEN_HEIGHT / 2,
+  });
+  Matter.Body.setVelocity(entities.Player.body, { x: 0, y: 0 });
+
+  for (let i = 1; i <= 3; i++) {
+    const box = entities[`Obstacle${i}`];
+
+    let x;
+
+    switch (i) {
+      case 1:
+        x = Constants.SCREEN_WIDTH + Constants.OBSTACLE_HEIGHT / 2;
+        break;
+      case 2:
+        x = Constants.SCREEN_WIDTH * 1.385 + Constants.OBSTACLE_HEIGHT / 2;
+        break;
+      case 3:
+        x = Constants.SCREEN_WIDTH * 1.77 + Constants.OBSTACLE_HEIGHT / 2;
+        break;
+      default:
+        break;
+    }
+
+    Matter.Body.setPosition(box.body, {
+      x: x,
+      y: getY(),
+    });
+    box.isDangerous = false;
+    box.body.isDangerous = false;
+    engine.world.gravity.y = 0.5;
+
+    dispatch({ type: "pause" });
+  }
 }
